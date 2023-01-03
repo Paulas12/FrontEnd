d1b9a43178c1b5393e5d99e13ca84a79feb22762
Paula <paushuppe@gmail.com>
2023-01-03 15:21:54 -0300

    detalles

--- portfolio/src/app/componentes/edit-angular/edit-angular.component.ts
+++ portfolio/src/app/componentes/edit-angular/edit-angular.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-edit-angular',
   templateUrl: './edit-angular.component.html',
   styleUrls: ['./edit-angular.component.css']
 })
-export class EditAngularComponent {
-
+export class EditAngularComponent{
+  
 }
